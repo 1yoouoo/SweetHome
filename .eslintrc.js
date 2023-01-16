0bc145a0a9a2974e5c01fa183a589a025e193e0e
이지윤 <1yoouoo@gmail.com>
2023-01-16 16:26:44 +0900

    update: cra, eslint, gitignore, testcode

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+// .eslintrc.js example
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
