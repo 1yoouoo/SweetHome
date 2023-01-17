262a7296cb1ab6d42bd2264afaa4e8c4cb725a16
이지윤 <1yoouoo@gmail.com>
2023-01-17 12:02:33 +0900

    design: add logo

---
+++ src/Views/Logo/WhiteLogo.tsx
@@ -0,0 +1,13 @@
+import logo from "/Users/blanc/Documents/Project/sns/src/Assets/logo.png";
+
+const WhiteLogo = () => {
+  return (
+    <>
+      <span>
+        <img id="img" src={logo} className="App-logo" alt="logo" />
+      </span>
+    </>
+  );
+};
+
+export default WhiteLogo;
