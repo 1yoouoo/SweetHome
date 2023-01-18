295e5adf769270fcb1f7f56b709e26e574ec4331
이지윤 <1yoouoo@gmail.com>
2023-01-18 17:29:17 +0900

    design: PostItem

--- src/Views/PostItem/PostItem.tsx
+++ src/Views/PostItem/PostItem.tsx
@@ -1,6 +1,8 @@
 import EllipsisSvg from "../../Assets/SVG/EllipsisSvg";
+import InteractionBar from "../InteractionBar/InteractionBar";
 import "./PostItem.scss";
 import sample from "/Users/blanc/Documents/Project/sns/src/Assets/sample.png";
+// import sample from "/Users/blanc/Documents/Project/sns/src/Assets/vertical_sample.jpeg";
 
 const PostItem = () => {
   return (
@@ -8,7 +10,7 @@ const PostItem = () => {
       <div className="PostItem__top">
         <span className="PostItem__top--left">
           <span>img</span>
-          <span>nickname</span>
+          <span>NICK_NAME</span>
         </span>
         <span className="PostItem__top--right">
           <EllipsisSvg />
@@ -17,7 +19,16 @@ const PostItem = () => {
       <div className="PostItem__main">
         <img src={sample} alt="sample" />
       </div>
-      <div className="PostItem__bottom">hi</div>
+      <div className="PostItem__bottom">
+        <InteractionBar />
+        <div className="PostItem__bottom--likes">9 likes</div>
+        <div className="PostItem__bottom--content">
+          <span>NICK_NAME</span>
+          <span>TEXT_TEXT_TEXT_TEXT_TEXT_TEXT_</span>
+        </div>
+        <div className="PostItem__bottom--comments">View all 3 comments</div>
+        <div className="PostItem__bottom--posted-at">17 HOURS AGO</div>
+      </div>
     </section>
   );
 };
