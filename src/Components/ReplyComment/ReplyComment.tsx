d687763e81f2d694d1298bf80952559bd79877e1
이지윤 <1yoouoo@gmail.com>
2023-02-16 17:15:44 +0900

    feat: add recomment

--- src/Components/ReplyComment/ReplyComment.tsx
+++ src/Components/ReplyComment/ReplyComment.tsx
@@ -1,7 +1,54 @@
+import { useEffect } from "react";
+import EllipsisSvg from "../../Assets/SVG/EllipsisSvg";
+import SingleDotSvg from "../../Assets/SVG/SingleDotSvg";
+import SmallHeartSvg from "../../Assets/SVG/SmallHeartSvg";
+import UserPhoto from "../../sass/styled-components/UserPhoto";
+import { timeFormat } from "../../utills/function/function";
 import "./ReplyComment.scss";
 
-function ReplyComment() {
-  return <div className="ReplyComments">asd</div>;
+function ReplyComment({ replyComment }: any) {
+  useEffect(() => {
+    console.log(replyComment);
+  }, []);
+
+  return (
+    <li className="PostComment">
+      <span className="PostComment__wrapper">
+        <span className="PostComment__left">
+          <UserPhoto
+            size="44px"
+            userProfileImage={replyComment.userProfileImageUrl}
+          />
+        </span>
+        <div className="PostComment__center">
+          <div className="PostComment__center--wrapper">
+            <span className="PostComment__center--wrapper-username">
+              {replyComment.nickName}
+            </span>
+            <span className="PostComment__center--wrapper-dot">
+              <SingleDotSvg />
+            </span>
+            <span className="PostComment__center--wrapper-created-at">
+              {timeFormat(replyComment.updatedAt)}
+            </span>
+          </div>
+          <div className="PostComment__center--text">
+            {replyComment.content}
+          </div>
+        </div>
+      </span>
+      {/* <span className="PostComment__right">
+    <SmallHeartSvg />
+    <span className="PostComment__right--dot" onClick={onClickDot}>
+      <span className={dotToggle ? "activated_dot" : "hidden_dot"}>
+        <span onClick={onClickEdit}>수정</span>
+        <span onClick={deleteComment}>삭제</span>
+      </span>
+      <EllipsisSvg />
+    </span>
+  </span> */}
+    </li>
+  );
 }
 
 export default ReplyComment;
--- src/Components/ReplyComments/ReplyComments.tsx
+++ src/Components/ReplyComments/ReplyComments.tsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
 import API from "../../API/API";
+import CirclePlus from "../../Assets/SVG/CirclePlus";
+import ReplyComment from "../ReplyComment/ReplyComment";
 import "./ReplyComments.scss";
 interface ReplyCommentsProps {
   commentId: number | undefined;
 }
+export interface ReplyCommentType {
+  content?: string;
+  reCommentId?: number;
+  nickName: string;
+  updatedAt?: string | Date;
+  userProfileImageUrl?: string;
+  reCommentLikeSize?: number;
+  replyComment: any;
+}
 function ReplyComments({ commentId }: ReplyCommentsProps) {
   const [currentPage, setCurrentPage] = useState(0);
+  const [lastId, setLastId] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
   const [replyComments, setReplyComments] = useState<any>();
-  const fetchReplyComments = async (reCommentId: number) => {
+
+  const fetchReplyComments = async (reCommentId?: number) => {
     const response = await API.getReplyComments({ commentId, reCommentId });
     return response.data.data.reCommentListResponse;
   };
   const getReplyComments = async () => {
-    const response = await fetchReplyComments(currentPage);
+    const response = await fetchReplyComments();
     setReplyComments(response.reCommentListDetailResponseList);
     setIsLastPage(!response.hasNext);
+  };
+  const getMoreReplyComments = async () => {
+    const response = await fetchReplyComments(lastId);
+    setReplyComments([...replyComments, ...response.commentListDetailResponse]);
+    setIsLastPage(response.hasNext);
+    // setLastId(response.commentListDetailResponse.at(-1)?.commentId);
   };
   useEffect(() => {
     console.log("Mount!");
@@ -24,8 +43,13 @@ function ReplyComments({ commentId }: ReplyCommentsProps) {
   }, []);
   return (
     <ul className="ReplyComments">
-      {replyComments?.map((replyComment: any) => {
-        return <span>hi</span>;
+      {replyComments?.map((replyComment: ReplyCommentType) => {
+        return (
+          <ReplyComment
+            replyComment={replyComment}
+            key={replyComment.reCommentId}
+          />
+        );
       })}
     </ul>
   );
